62f712a5f2dc0f8c589569943718da1d48517973
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-02-10 19:34:04 -0500

    changed version

---
+++ R_Singh_MyFavoriteCars/src/app/helper-files/content-list.ts
@@ -0,0 +1,15 @@
+class ContentList{
+    private item: Content[] ;
+    constructor(item: Content){
+        this.item = [];
+        this.item[1] = item;
+        this.increaseCount();
+    }
+    get items(): Content[]{
+        return this.item;
+        }
+
+        increaseCount(){
+            return this.item;
+        }
+}
