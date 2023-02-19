bc2cc97b19b9e9bdb536043d7b02f0d2ff8ea95f
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-02-18 20:24:17 -0500

    added aan array of 6 components in the content-list component file

--- R_Singh_MyFavoriteCars/src/app/content-list/content-list.component.ts
+++ R_Singh_MyFavoriteCars/src/app/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
-
+contentArray : Content[];
+constructor(){
+  this.contentArray = [
+    {
+      id: 1 ,
+      title : 'Range Rover',
+      description : 'The Land Rover Range Rover (generally known simply as the Range Rover) is a 4x4 motor car produced by Land Rover, a marque and sub-brand of Jaguar Land Rover. The Range Rover line was launched in 1970 by British Leyland and is now in its fifth generation.',
+      creator:'British Leyland',
+      imgURL : 'https://en.wikipedia.org/wiki/Range_Rover',
+      type : 'Range Rover AutoBiography'
+  }
+  ]
+}
 }
