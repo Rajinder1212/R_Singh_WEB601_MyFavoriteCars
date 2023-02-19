8daba1210a16a8df01d31441b2247dfac9a5e01b
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-02-18 21:02:57 -0500

    added 5 different array elements

--- R_Singh_MyFavoriteCars/src/app/content-list/content-list.component.ts
+++ R_Singh_MyFavoriteCars/src/app/content-list/content-list.component.ts
@@ -17,7 +17,40 @@ constructor(){
       creator:'British Leyland',
       imgURL : 'https://en.wikipedia.org/wiki/Range_Rover',
       type : 'Range Rover AutoBiography'
-  }
+  },
+  {
+    id: 2 ,
+    title : 'Honda',
+    description : 'Soichiro Honda (本田 宗一郎, Honda Sōichirō, 17 November 1906 – 5 August 1991) was a Japanese engineer and industrialist.In 1948, he established Honda Motor Co., Ltd. and oversaw its expansion from a wooden shack manufacturing bicycle motors to a multinational automobile and motorcycle manufacturer.',
+    creator:'Soichiro Honda',
+    imgURL : 'https://en.wikipedia.org/wiki/Soichiro_Honda',
+    type : 'Honda Motor Cp '
+}, 
+{
+  id: 3 ,
+  title : 'Elantra',
+  description : 'Elantra got a slightly roomier interior and a more sophisticated infotainment system. The standard engine is a 147-hp four-cylinder teamed with a continuously variable transmission',
+  creator:'Luc Donckerwolke',
+  imgURL : 'https://en.wikipedia.org/wiki/Hyundai_Elantra',
+  type : 'Hyundai Elantra  '
+},
+
+{
+  id: 4 ,
+  title : 'Civic',
+  description : ' high market availability and the opportunities offered by its lightweight chassis',
+  creator:' Honda',
+  imgURL : 'https://en.wikipedia.org/wiki/Honda_Civic',
+  type : 'Honda Civic'
+},
+{
+  id: 5 ,
+  title : 'Corvette',
+  description : 'With eight design generations, noted sequentially from C1 to C8, the Corvette is noted for its performance and distinctive fiberglass or composite panel',
+  creator:'Harley Earl',
+  imgURL : 'https://en.wikipedia.org/wiki/Chevrolet_Corvette',
+  type : 'Chevrolet Corvette'
+},
   ]
 }
 }
