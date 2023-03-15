d7550d66d4c8e17499c93c23c44d7614a6cfc0b5
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-03-15 07:01:55 -0500

    created a new component

--- R_Singh_MyFavoriteCars/src/app/app.module.ts
+++ R_Singh_MyFavoriteCars/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { CreateContentComponent } from './create-content/create-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent
+    ContentCardComponent,
+    CreateContentComponent
   ],
   imports: [
     BrowserModule
