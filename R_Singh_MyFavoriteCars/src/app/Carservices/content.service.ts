67ffd6b3f1bda73811e76808f804b8f5dd7fd656
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-03-16 12:59:52 -0500

    added an observerable

--- R_Singh_MyFavoriteCars/src/app/Carservices/content.service.ts
+++ R_Singh_MyFavoriteCars/src/app/Carservices/content.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { contentArray } from '../helper-files/contentDb';
+import { Content } from '../helper-files/content-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor() {}
+    getContent() : Observable<Content[]>{
+      return of(contentArray)
+    }
 }
