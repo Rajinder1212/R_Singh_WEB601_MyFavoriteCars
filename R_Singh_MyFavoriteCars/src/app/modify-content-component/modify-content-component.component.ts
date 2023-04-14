c2242153b4684272f7e69b11bc9d79ddc08b4dcf
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-04-14 14:32:10 -0500

    updated modify.content.html

--- R_Singh_MyFavoriteCars/src/app/modify-content-component/modify-content-component.component.ts
+++ R_Singh_MyFavoriteCars/src/app/modify-content-component/modify-content-component.component.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Content } from '../content';
+import { ContentService } from '../Carservices/content.service'
+
 
 @Component({
-  selector: 'app-modify-content-component',
-  templateUrl: './modify-content-component.component.html',
-  styleUrls: ['./modify-content-component.component.scss']
+  selector: 'app-component-list',
+  templateUrl: './component-list.component.html',
+  styleUrls: ['./component-list.component.scss']
 })
 
-export class ModifyContentComponentComponent {
-  contentsArray: any;
+export class ComponentListComponent {
+  contentListItems: Content[]=[];
+
+  constructor(private contentService: ContentService){
+  }
+
+  clickHandler(index:number){
+    console.log('item id is:' + this.contentListItems[index].id);
+    console.log('item title is:' + this.contentListItems[index].title);
+  };
+
+  ngOnInit() {
+
+    this.contentService.getContent().subscribe(content => this.contentListItems = content);
+    }
+  
   addContentToList(newContentItem: Content): void {
-    this.contentsArray.addContent(newContentItem)
-    .subscribe((newContentFromServer: any) =>
-    this.contentsArray.push(newContentFromServer)
+    this.contentService.addContent(newContentItem)
+    .subscribe((newContentFromServer: Content) =>
+    this.contentListItems.push(newContentFromServer)
     );
-}
+  }
+    
+  updateContentInList(contentItem: Content): void {
+    this.contentService.updateContent(contentItem)
+    .subscribe(() =>
+    console.log("Content updated successfully")
+    );
+  }   
+  
+  clickEvent(v:string){
+    alert(v);
+  }
 }
