33c7486523570d5a60bd129dce29de5dc1a27131
Rajinder1212 <105305480+Rajinder1212@users.noreply.github.com>
2023-04-14 13:44:16 -0500

    add in the necessary inputs for each field of your Content, EXCLUDING the id, and add a button below the inputs

--- R_Singh_MyFavoriteCars/src/app/modify-content-component/modify-content-component.component.ts
+++ R_Singh_MyFavoriteCars/src/app/modify-content-component/modify-content-component.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Content } from '../content';
 
 @Component({
   selector: 'app-modify-content-component',
   templateUrl: './modify-content-component.component.html',
   styleUrls: ['./modify-content-component.component.scss']
 })
-export class ModifyContentComponentComponent {
 
+export class ModifyContentComponentComponent {
+  contentsArray: any;
+  addContentToList(newContentItem: Content): void {
+    this.contentsArray.addContent(newContentItem)
+    .subscribe((newContentFromServer: any) =>
+    this.contentsArray.push(newContentFromServer)
+    );
+}
 }
